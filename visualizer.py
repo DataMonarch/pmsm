f4a03e574a0388148f796736ce152f42dd8bb23f
DataMonarch <toghrultahirov2@gmail.com>
2022-01-19 17:33:34 +0400

    imported necessary dependencies

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import train_test_split
 
 sns.set_style("darkgrid")
 plt.style.use('seaborn')
