5fa602e0a7bacd84ae020fcc11cc94556176b80e
DataMonarch <toghrultahirov2@gmail.com>
2022-01-19 18:01:33 +0400

    added callback

--- master.py
+++ master.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from data_preprocessing import seq_data_preprocessing, split_sequence_train_test
+from cnn_builder import cnn_model_builder
 
 target_features = ['motor_speed', 'torque', 'stator_yoke', 'stator_tooth', 'stator_winding']
 input_features = ['u_q', 'u_d', 'i_q', 'i_d', 'coolant', 'ambient']
@@ -8,3 +10,24 @@ batch_size = 500
 
 # data can be found in this kaggle repo
 # "https://www.kaggle.com/wkirgsn/electric-motor-temperature?select=measures_v2.csv"
+
+cnn_model = cnn_model_builder(seq_len, n_features=6, n_targets=5, kernel_size=2)
+cnn_model.summary()
+
+training_history = []
+
+epochs = 100
+
+model_es_callback = tf.keras.callbacks.EarlyStopping(
+#     filepath='../output/',
+#     save_weights_only=True,
+    monitor='val_mean_squared_error',
+    min_delta = 0.0001,
+    patience = 10,
+    verbose = 1,
+    mode='min',
+    restore_best_weights=True)
+
+
+history = cnn_model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2, callbacks=[model_es_callback])
+training_history.append(history)
