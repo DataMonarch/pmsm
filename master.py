425cdab3613e1816917cbeebb1ecee8497442f38
DataMonarch <toghrultahirov2@gmail.com>
2022-01-19 17:28:35 +0400

    default sequence length(seq_len) and batch size(size) included for reproducibility

--- master.py
+++ master.py
@@ -1,4 +1,10 @@
-
+import pandas as pd
 
 target_features = ['motor_speed', 'torque', 'stator_yoke', 'stator_tooth', 'stator_winding']
 input_features = ['u_q', 'u_d', 'i_q', 'i_d', 'coolant', 'ambient']
+
+seq_len = 256
+batch_size = 500
+
+# data can be found in this kaggle repo
+# "https://www.kaggle.com/wkirgsn/electric-motor-temperature?select=measures_v2.csv"
